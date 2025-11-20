eea7c698a4aa03dbb8ef3bd06d355dba26c38beb
SirokiA <sirokia@kkszki.hu>
2025-11-20 19:29:41 +0100

    almost done

--- viete.js
+++ viete.js
@@ -1,4 +1,25 @@
 document.getElementById("button1").onclick = () =>{
-    var a =
-    alert("Hello world!")
+    var a = document.getElementById("inputa").value;
+    var b = document.getElementById("inputb").value;
+    var c = document.getElementById("inputc").value;
+
+    var D = b*b-4*a*c;
+    var eredmeny = "" ;
+    if (D < 0){
+        eredmeny = "Nincs megoldás";
+    }
+
+    else if (D==0) {
+
+        eredmeny = "Egy megoldása van";
+    }
+
+    else {
+        eredmeny = "Két megoldása van";
+    }
+    alert("D");
+
+
+    document.getElementById("span1").innerHTML = eredmeny;
 }
+
